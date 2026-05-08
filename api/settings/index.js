b44b34fe6230b656ec3658c0729e399840c89d17
LiVE0111 <48410551+LiVE0111@users.noreply.github.com>
2026-05-08 11:33:31 +0700

    Update index.js

--- api/settings/index.js
+++ api/settings/index.js
@@ -23,9 +23,7 @@ const KEY_MAP = {
   qrNotice:        'QR_NOTICE',
   bankName:        'BANK_NAME',
   bankAccount:     'BANK_ACCOUNT',
-  bankAccountName: 'BANK_ACCOUNT_NAME',
-  ref1Format:      'REF1_FORMAT',     // STUDENT_ID / ID_CARD
-  ref2Format:      'REF2_FORMAT'      // CLASS_CODE / CLASS_TERM_YEAR
+  bankAccountName: 'BANK_ACCOUNT_NAME'
 };
 
 module.exports = async function handler(req, res) {
@@ -113,9 +111,7 @@ module.exports = async function handler(req, res) {
         qrNotice:        map.QR_NOTICE        || '',
         bankName:        map.BANK_NAME        || '',
         bankAccount:     map.BANK_ACCOUNT     || '',
-        bankAccountName: map.BANK_ACCOUNT_NAME || '',
-        ref1Format:      map.REF1_FORMAT      || 'STUDENT_ID',
-        ref2Format:      map.REF2_FORMAT      || 'CLASS_CODE'
+        bankAccountName: map.BANK_ACCOUNT_NAME || ''
       });
     } catch (e) {
       return fail(res, e.message, 500);
